7192e4f0ff0429483b5e249f2dcefe0045ab0eb8
Ubuntu <flaconumero2@gmail.com>
2025-02-25 00:26:06 +0000

    Delete stop script

--- parse_list.js
+++ parse_list.js
@@ -1,8 +1,11 @@
 #!/home/ubuntu/.nvm/versions/node/v22.13.1/bin/node
 
+let current = new Date().getTime();
 let json = process.argv[2];
 json = JSON.parse(json).data.Page.mediaList;
+console.log(json);
 json = json.filter( entry => entry.media.nextAiringEpisode );
+console.log(json);
 
 // 3 seconds is the maximum I will allow two timestamps to be apart for equality.
 let tolerance = 3000; 
@@ -10,8 +13,7 @@ let tolerance = 3000;
 json.forEach(j => {
 	let name = j.media.title.english
 	let episode = j.media.nextAiringEpisode.episode;
-	let timestamp = j.media.nextAiringEpisode.airingAt * 1000;
-	let current = new Date().getTime();
+	let timestamp = j.media.nextAiringEpisode.airingAt * 1000;	
 	if(Math.abs(current - timestamp) <= tolerance)
 		fetch('http://localhost:8080/notify', {method: 'POST', body: JSON.stringify({"name": name, "episode": episode}), headers: {'Content-Type': 'application/json'}});
 });
--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 source /home/ubuntu/anime_notifications/variables
-if ! pgrep -f "node index.js" > /dev/null; then
-	node index.js
-	sleep 2
-fi
+node index.js
