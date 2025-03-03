ffa247015e5c30ef37584c25691a31aff267f371
Ubuntu <flaconumero2@gmail.com>
2025-03-03 12:13:28 +0000

    Fix indentations in js files

--- index.js
+++ index.js
@@ -1,53 +1,50 @@
-import {Client, Events, GatewayIntentBits } from 'discord.js';
-import express from 'express'; 
-const client = new Client({ intents: [GatewayIntentBits.Guilds]});
+import {Client, Events, GatewayIntentBits} from 'discord.js';
+import express from 'express';
+const client = new Client({intents : [ GatewayIntentBits.Guilds ]});
 let user;
 client.on(Events.ClientReady, async rc => {
-	console.log('hello from discord bot');
-	user = await rc.users.fetch(process.env.USER_ID);
-	client.user.setPresence({status: 'online'});
+    console.log('hello from discord bot');
+    user = await rc.users.fetch(process.env.USER_ID);
+    client.user.setPresence({status : 'online'});
 });
 client.on(Events.InteractionCreate, async interaction => {
-	let n = interaction.options.data[0].value;
-	if(n > 100){
-		interaction.reply(`Error: \`n\` must be less than 100. You entered: \`${n}\`.`);
-		return;
-	}
-	interaction.reply(`Deleting \`${n}\` messages...`);
-	let gian;
-	try{
-		gian = await client.users.fetch('247492668131770369');
-	}catch(e){
-		console.log('Error with API request fetching my account:');
-		console.log(e);
-		return;
-	}
-	try{
-		let channel = await gian.createDM();
-		let messages = await channel.messages.fetch({limit: n});
-		messages.filter(msg => msg.author.id == client.user.id);
-		await Promise.all(messages.map(msg => msg.delete()));
-	}catch(e){
-		console.log('Error with purging DM channel:');
-		console.log(e);
-		return;
-	}
+    let n = interaction.options.data[0].value;
+    if (n > 100) {
+        interaction.reply(
+            `Error: \`n\` must be less than 100. You entered: \`${n}\`.`);
+        return;
+    }
+    interaction.reply(`Deleting \`${n}\` messages...`);
+    let gian;
+    try {
+        gian = await client.users.fetch('247492668131770369');
+    } catch (e) {
+        console.log('Error with API request fetching my account:');
+        console.log(e);
+        return;
+    }
+    try {
+        let channel = await gian.createDM();
+        let messages = await channel.messages.fetch({limit : n});
+        messages.filter(msg => msg.author.id == client.user.id);
+        await Promise.all(messages.map(msg => msg.delete()));
+    } catch (e) {
+        console.log('Error with purging DM channel:');
+        console.log(e);
+        return;
+    }
 });
 const app = express();
 app.use(express.json());
-app.get('/', (req, res) => {
-	res.send('hello :3');
-});
+app.get('/', (req, res) => { res.send('hello :3'); });
 app.post('/notify', async (req, res) => {
-	let json = req.body;
-	console.log(json);
-	let name = json.name;
-	let episode = json.episode;
-	user.send(`Episode ${episode} of ${name} has aired!`);
-	res.send('OK');
+    let json = req.body;
+    console.log(json);
+    let name = json.name;
+    let episode = json.episode;
+    user.send(`Episode ${episode} of ${name} has aired!`);
+    res.send('OK');
 });
 
-app.listen(8080, () => {
-	console.log('Anime notifications bot online.');
-});
+app.listen(8080, () => { console.log('Anime notifications bot online.'); });
 client.login(process.env.ANILIST_BOT_TOKEN);
--- parse_list.js
+++ parse_list.js
@@ -4,16 +4,21 @@ let current = new Date().getTime();
 let json = process.argv[2];
 json = JSON.parse(json).data.Page.mediaList;
 console.log(json);
-json = json.filter( entry => entry.media.nextAiringEpisode );
+json = json.filter(entry => entry.media.nextAiringEpisode);
 console.log(json);
 
-// 8 seconds is the maximum I will allow two timestamps to be apart for equality.
-let tolerance = 8000; 
+// 8 seconds is the maximum I will allow two timestamps to be apart for
+// equality.
+let tolerance = 8000;
 
 json.forEach(j => {
-	let name = j.media.title.english
-	let episode = j.media.nextAiringEpisode.episode;
-	let timestamp = j.media.nextAiringEpisode.airingAt * 1000;	
-	if(Math.abs(current - timestamp) <= tolerance)
-		fetch('http://localhost:8080/notify', {method: 'POST', body: JSON.stringify({"name": name, "episode": episode}), headers: {'Content-Type': 'application/json'}});
+    let name = j.media.title.english
+    let episode = j.media.nextAiringEpisode.episode;
+    let timestamp = j.media.nextAiringEpisode.airingAt * 1000;
+    if (Math.abs(current - timestamp) <= tolerance)
+        fetch('http://localhost:8080/notify', {
+            method : 'POST',
+            body : JSON.stringify({"name" : name, "episode" : episode}),
+            headers : {'Content-Type' : 'application/json'}
+        });
 });
